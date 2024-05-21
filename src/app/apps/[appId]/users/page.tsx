6f52d175cf001e09013461b9099afab50eb8b13d
rosaga <richardjohnzranger@gmail.com>
2024-05-21 12:43:54 +0300

    intergration of users with backend

--- src/app/apps/[appId]/users/page.tsx
+++ src/app/apps/[appId]/users/page.tsx
@@ -1,17 +1,20 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Box from "@mui/material/Box";
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
 import IosShareIcon from "@mui/icons-material/IosShare";
-import PeakButton from "../../../../components/button/button";
 import AddIcon from '@mui/icons-material/Add';
+import * as XLSX from 'xlsx';
+import PeakButton from "../../../../components/button/button";
 import PeakSearch from "../../../../components/search/search"
 import InviteUserModal from "../../../../components/modal/inviteUser"
 
 const Users = () => {
-
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [rows, setRows] = useState<GridRowsProp>([]);
+  const [loading, setLoading] = useState(true);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -20,55 +23,58 @@ const Users = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-  
-  const filterOptions = [
-    { value: 'eq__external_id', label: 'Phone Number' },
-    { value: 'ilike__first_name', label: 'Created By' },
-    { value: 'ilike__last_name', label: 'Last Name' },
-];
 
-  const rows: GridRowsProp = [
-    {
-      id: 1,
-      first_name: "Allan",
-      last_name: "Oluoch",
-      email: "allan@gmail.com",
-    },
-    {
-      id: 2,
-      first_name: "Richard",
-      last_name: "Osaga",
-      email: "osaga@gmail.com",
-    },
-    {
-      id: 3,
-      first_name: "Victor",
-      last_name: "Siderra",
-      email: "siderra@gmail.com",
-    },
-    {
-      id: 3,
-      first_name: "Ryan",
-      last_name: "Ashiruma",
-      email: "ashiruma@gmail.com",
-    },
+  const filterOptions = [
+    { value: '', label: 'All Categories' },
+    { value: 'ilike__first_name', label: 'First Name' },
+    { value: 'ilike__last_name', label: 'Last Name' },
+    { value: 'ilike__email', label: 'Email' },
+    
   ];
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const authResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/public/token`, {
+        username: process.env.NEXT_PUBLIC_USERNAME,
+        password: process.env.NEXT_PUBLIC_PASSWORD
+      });
+
+      const token = authResponse.data.token; 
+      const usersResponse = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/organization/${process.env.NEXT_PUBLIC_ORG_ID}/user/list`, {        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      const users = usersResponse.data.map((user, index) => ({
+        id: user.user_id, 
+        first_name: user.firstname,
+        last_name: user.lastname,
+        email: user.email
+      }));
+
+      setRows(users);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching users data:", error);
+      setLoading(false);
+    }
+  };
+
+  const exportToExcel = () => {
+    const worksheet = XLSX.utils.json_to_sheet(rows);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Users");
+    XLSX.writeFile(workbook, "users_data.xlsx");
+  };
 
   const columns: GridColDef[] = [
     { field: "first_name", headerName: "First Name", flex: 1 },
     { field: "last_name", headerName: "Last Name", flex: 1 },
     { field: "email", headerName: "Email", flex: 1 },
   ];
-
-  const data = [
-    ["Allan", "Oluoch", "allan@gmail.com"],
-    ["Richard", "Osaga", "osaga@gmail.com"],
-    ["Victor", "Siderra", "siderra@gmail.com"],
-    ["Ryan", "Ashiruma", "ashiruma@gmail.com"],
-  ];
-
-  const selectedFilter = "Email"
-    const searchInput = "sid@gmail.com"
 
   return (
     <div className="p-4 sm:ml-64 h-screen ">
@@ -89,25 +95,29 @@ const Users = () => {
                   buttonText="Export"
                   icon={IosShareIcon}
                   className="rounded-[2px] border-2 text-sm px-2 py-1 shadow-sm outline-none"
-                  onClick={openModal}
+                  onClick={exportToExcel} // Update the onClick handler to call exportToExcel
                 />
               </div>
             </div>
 
             <div className="mt-4">
               <div style={{ height: 350, width: "100%" }}>
-                <DataGrid
-                  rows={rows}
-                  columns={columns}
-                  sx={{
-                    "& .MuiDataGrid-columnHeader": {
-                      backgroundColor: "#E5E4E2",
-                    },
-                    "&.MuiDataGrid-root": {
-                      border: "none",
-                    },
-                  }}
-                />
+                {loading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    sx={{
+                      "& .MuiDataGrid-columnHeader": {
+                        backgroundColor: "#E5E4E2",
+                      },
+                      "&.MuiDataGrid-root": {
+                        border: "none",
+                      },
+                    }}
+                  />
+                )}
               </div>
             </div>
           </div>
