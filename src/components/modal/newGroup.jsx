d549eea781b4c45809d4be6748deb4323de476f5
rosaga <richardjohnzranger@gmail.com>
2024-06-03 12:20:19 +0300

    addition of new group

--- src/components/modal/newGroup.jsx
+++ src/components/modal/newGroup.jsx
@@ -1,23 +1,79 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const NewGroupModal = ({ closeModal }) => {
+  const [groupName, setGroupName] = useState("");
+  const [csvFile, setCsvFile] = useState(null);
+  const [description, setDescription] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-          if (event.target.id === "authentication-modal") {
-            closeModal();
+  const handleRequest = async () => {
+    try {
+      const authResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/public/token`, {
+        username: process.env.NEXT_PUBLIC_USERNAME,
+        password: process.env.NEXT_PUBLIC_PASSWORD,
+      });
+
+      const fetchedToken = authResponse.data.token;
+      console.log(groupName);
+
+      try {
+        const groupResponse = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/organization/${process.env.NEXT_PUBLIC_ORG_ID}/group/`,
+          {
+            "name": groupName,
+            // "description": description,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${fetchedToken}`,
+              "Content-Type": "application/json",
+            },
           }
-        };
-    
-        window.addEventListener("click", handleClickOutside);
-    
-        return () => {
-          window.removeEventListener("click", handleClickOutside);
-        };
-      }, [closeModal]);
+        );
+        console.log(groupResponse.data);
+        const groupId = groupResponse.data.id;
 
-      
+        // Prepare form data for file upload
+        const formData = new FormData();
+        formData.append("contacts", csvFile);
+        formData.append("group_id", groupId);
+
+        await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/organization/${process.env.NEXT_PUBLIC_ORG_ID}/contact/upload`,
+          formData,
+          {
+            headers: {
+              Authorization: `Bearer ${fetchedToken}`,
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+
+        setSuccessMessage(`The group ${groupName} has been created and CSV file uploaded successfully.`);
+      } catch (error) {
+        console.error("Error Creating Group or Uploading CSV File", error);
+      }
+    } catch (error) {
+      console.error("Error fetching token:", error);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.id === "authentication-modal") {
+        closeModal();
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [closeModal]);
+
   return (
     <div
       id="authentication-modal"
@@ -27,87 +83,103 @@ const NewGroupModal = ({ closeModal }) => {
     >
       <div className="relative p-4 w-full max-w-2xl max-h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-              New Group
-            </h3>
-            <button
-              type="button"
-              className="end-2.5 bg-transparent text-orange-400 border-[1.5px] border-orange-400 rounded-lg text-sm w-52 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              + Add New Group
-            </button>
-          </div>
-          <div className="p-4 md:p-5">
-            <form className="space-y-2" action="#">
-            <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Group Name
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="Group 1"
-                  required
-                />
+          {successMessage ? (
+            <div className="p-4 text-center">
+              <div className="mb-4 text-2xl font-semibold text-green-500">
+                Success!
               </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Upload CSV File
-                </label>
-                <input
-                  type="file"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="rasanga"
-                  required
-                />
+              <div className="mb-4 text-gray-900 dark:text-white">
+                {successMessage}
               </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="description"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Description
-                </label>
-                <textarea
-                  type="description"
-                  name="description"
-                  id="description"
-                  row="5"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="Description for the Group"
-                  required
-                />
+              <button
+                onClick={() => {
+                  setSuccessMessage("");
+                  closeModal();
+                }}
+                className="w-full text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                OK
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  New Group
+                </h3>
               </div>
-              
-              <div className="flex space-x-2">
-                  <button
-                    type="submit"
-                    className="w-full text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full text-white bg-orange-400 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-                    onClick={closeModal}
-                  >
-                    Submit
-                  </button>
-                </div>
-
-            </form>
-          </div>
+              <div className="p-4 md:p-5">
+                <form className="space-y-2" action="#">
+                  <div>
+                    <label
+                      htmlFor="groupName"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Group Name
+                    </label>
+                    <input
+                      type="text"
+                      name="groupName"
+                      id="groupName"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="Group 1"
+                      onChange={(e) => setGroupName(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="csvFile"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Upload CSV File
+                    </label>
+                    <input
+                      type="file"
+                      name="csvFile"
+                      id="csvFile"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      onChange={(e) => setCsvFile(e.target.files[0])}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="description"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Description
+                    </label>
+                    <textarea
+                      name="description"
+                      id="description"
+                      rows="5"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="Description for the Group"
+                      onChange={(e) => setDescription(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="flex space-x-2">
+                    <button
+                      type="button"
+                      className="w-full text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      onClick={closeModal}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="w-full text-white bg-orange-400 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                      onClick={handleRequest}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
