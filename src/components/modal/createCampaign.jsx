4ea33710f15838e1f3282aee978bbae8a37eee6c
rosaga <richardjohnzranger@gmail.com>
2024-06-03 12:19:36 +0300

    addition of create campaign api

--- src/components/modal/createCampaign.jsx
+++ src/components/modal/createCampaign.jsx
@@ -1,23 +1,104 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const CreateCampaignModal = ({ closeModal }) => {
+  const [token, setToken] = useState(null);
+  const [groups, setGroups] = useState([]);
+  const [bundles, setBundles] = useState([]);
+  const [campaignName, setCampaignName] = useState("");
+  const [selectedGroup, setSelectedGroup] = useState("");
+  const [selectedBundle, setSelectedBundle] = useState("");
+  const [description, setDescription] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-          if (event.target.id === "authentication-modal") {
-            closeModal();
-          }
-        };
-    
-        window.addEventListener("click", handleClickOutside);
-    
-        return () => {
-          window.removeEventListener("click", handleClickOutside);
-        };
-      }, [closeModal]);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.id === "authentication-modal") {
+        closeModal();
+      }
+    };
 
-      
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [closeModal]);
+
+  useEffect(() => {
+    fetchToken();
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      fetchGroupsAndBundles();
+    }
+  }, [token]);
+
+  const fetchToken = async () => {
+    try {
+      const authResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/public/token`, {
+        username: process.env.NEXT_PUBLIC_USERNAME,
+        password: process.env.NEXT_PUBLIC_PASSWORD,
+      });
+
+      const fetchedToken = authResponse.data.token;
+      setToken(fetchedToken);
+    } catch (error) {
+      console.error("Error fetching token:", error);
+    }
+  };
+
+  const fetchGroupsAndBundles = async () => {
+    try {
+      const groupsResponse = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/organization/${process.env.NEXT_PUBLIC_ORG_ID}/group/list`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setGroups(groupsResponse.data);
+
+      const bundlesResponse = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/organization/${process.env.NEXT_PUBLIC_ORG_ID}/balance`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setBundles(bundlesResponse.data);
+    } catch (error) {
+      console.error("Error fetching groups or bundles:", error);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      name: campaignName,
+      group_id: parseInt(selectedGroup),
+      bundle: selectedBundle,
+      description: description,
+      content: message,
+    };
+
+    console.log("Creating campaign...", formData);
+    try {
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/v1/organization/${process.env.NEXT_PUBLIC_ORG_ID}/campaign/create`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      alert("Campaign created successfully.");
+      closeModal();
+    } catch (error) {
+      console.error("Error creating campaign:", error);
+    }
+  };
+
   return (
     <div
       id="authentication-modal"
@@ -45,10 +126,10 @@ const CreateCampaignModal = ({ closeModal }) => {
             </button>
           </div>
           <div className="p-4 md:p-5">
-            <form className="space-y-2" action="#">
-            <div className="mb-4">
+            <form className="space-y-2" onSubmit={handleSubmit}>
+              <div className="mb-4">
                 <label
-                  htmlFor="email"
+                  htmlFor="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Campaign Name
@@ -59,77 +140,111 @@ const CreateCampaignModal = ({ closeModal }) => {
                   id="name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                   placeholder="robina"
+                  value={campaignName}
+                  onChange={(e) => setCampaignName(e.target.value)}
                   required
                 />
               </div>
 
               <div className="mb-4">
                 <label
-                  htmlFor="email"
+                  htmlFor="group"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Select Group
                 </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
+                <select
+                  name="group"
+                  id="group"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="robina"
+                  value={selectedGroup}
+                  onChange={(e) => setSelectedGroup(e.target.value)}
                   required
-                />
+                >
+                  <option value="">Select Group</option>
+                  {groups.map((group) => (
+                    <option key={group.group_id} value={group.group_id}>
+                      {group.group_name} ({group.contacts})
+                    </option>
+                  ))}
+                </select>
               </div>
 
               <div className="mb-4">
                 <label
-                  htmlFor="email"
+                  htmlFor="bundle"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Select Bundle
+                </label>
+                <select
+                  name="bundle"
+                  id="bundle"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  value={selectedBundle}
+                  onChange={(e) => setSelectedBundle(e.target.value)}
+                  required
+                >
+                  <option value="">Select Bundle</option>
+                  {bundles.map((bundle) => (
+                    <option key={bundle.package} value={bundle.package}>
+                      {bundle.package}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="description"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Description
                 </label>
                 <textarea
-                  type="text"
-                  name="name"
-                  id="name"
+                  name="description"
+                  id="description"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                   placeholder="Enter Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
                   required
                 />
               </div>
 
               <div className="mb-4">
                 <label
-                  htmlFor="email"
+                  htmlFor="content"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Message
                 </label>
                 <textarea
-                  type="text"
-                  name="name"
-                  id="name"
+                  name="content"
+                  id="content"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                   placeholder="Enter Message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                   required
                 />
               </div>
-              
-              <div className="flex space-x-2">
-                  <button
-                    type="submit"
-                    className="w-full text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full text-white bg-orange-400 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-                    onClick={closeModal}
-                  >
-                    Submit
-                  </button>
-                </div>
 
+              <div className="flex space-x-2">
+                <button
+                  type="button"
+                  className="w-full text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  onClick={closeModal}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-orange-400 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                >
+                  Submit
+                </button>
+              </div>
             </form>
           </div>
         </div>
