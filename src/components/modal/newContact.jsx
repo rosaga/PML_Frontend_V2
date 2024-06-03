e9ef15a676e95a03170f7345d649255b89d3977d
rosaga <richardjohnzranger@gmail.com>
2024-06-03 12:19:59 +0300

    addition of new contcat api

--- src/components/modal/newContact.jsx
+++ src/components/modal/newContact.jsx
@@ -1,23 +1,62 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const NewContactModal = ({ closeModal }) => {
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-          if (event.target.id === "authentication-modal") {
-            closeModal();
+  const handleRequest = async () => {
+    try {
+      const authResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/public/token`, {
+        username: process.env.NEXT_PUBLIC_USERNAME,
+        password: process.env.NEXT_PUBLIC_PASSWORD,
+      });
+
+      const fetchedToken = authResponse.data.token;
+      try {
+        const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/organization/${process.env.NEXT_PUBLIC_ORG_ID}/contact/create`,
+          {
+            "mobile_no": phoneNumber,
+            "metadata": {
+              "firstname": firstname,
+              "lastname": lastname,
+            }
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${fetchedToken}`,
+              "Content-Type": "application/json",
+            },
           }
-        };
-    
-        window.addEventListener("click", handleClickOutside);
-    
-        return () => {
-          window.removeEventListener("click", handleClickOutside);
-        };
-      }, [closeModal]);
+        );
 
-      
+        setSuccessMessage(`The contact ${phoneNumber} has been created`);
+      } catch (error) {
+        console.error("Error Creating Contact", error);
+      }
+    } catch (error) {
+      console.error("Error fetching token:", error);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.id === "authentication-modal") {
+        closeModal();
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [closeModal]);
+
   return (
     <div
       id="authentication-modal"
@@ -27,55 +66,104 @@ const NewContactModal = ({ closeModal }) => {
     >
       <div className="relative p-4 w-full max-w-2xl max-h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-              New Contact
-            </h3>
-            <button
-              type="button"
-              className="end-2.5 bg-transparent text-orange-400 border-[1.5px] border-orange-400 rounded-lg text-sm w-52 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Download CSV Template
-            </button>
-          </div>
-          <div className="p-4 md:p-5">
-            <form className="space-y-2" action="#">
-            <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Phone Number
-                </label>
-                <input
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="+254711438911"
-                  required
-                />
+          {successMessage ? (
+            <div className="p-4 text-center">
+              <div className="mb-4 text-2xl font-semibold text-green-500">
+                Success!
               </div>
-
-              
-              <div className="flex space-x-2">
-                  <button
-                    type="submit"
-                    className="w-full text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full text-white bg-orange-400 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-                    onClick={closeModal}
-                  >
-                    Submit
-                  </button>
-                </div>
-
-            </form>
-          </div>
+              <div className="mb-4 text-gray-900 dark:text-white">
+                {successMessage}
+              </div>
+              <button
+                onClick={() => {
+                  setSuccessMessage("");
+                  closeModal();
+                }}
+                className="w-full text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                OK
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  New Contact
+                </h3>
+              </div>
+              <div className="p-4 md:p-5">
+                <form className="space-y-2" action="#">
+                  <div className="mb-4">
+                    <label
+                      htmlFor="phone"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Phone Number
+                    </label>
+                    <input
+                      type="text"
+                      name="phone"
+                      id="phone"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="+254711438911"
+                      onChange={(e) => setPhoneNumber(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="firstname"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      name="fname"
+                      id="firstname"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="First Name"
+                      onChange={(e) => setFirstName(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="lastname"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      name="lname"
+                      id="lastname"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="Last Name"
+                      onChange={(e) => setLastName(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="flex space-x-2">
+                    <button
+                      type="button"
+                      className="w-full text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      onClick={closeModal}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="w-full text-white bg-orange-400 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                      onClick={handleRequest}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
