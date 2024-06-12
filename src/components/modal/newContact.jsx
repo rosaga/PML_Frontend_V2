ad04d6a2cf6e05423d72c56f6e2b901c51c71428
vsidera <siderravictor@gmail.com>
2024-06-12 15:46:22 +0300

    fix issues

--- src/components/modal/newContact.jsx
+++ src/components/modal/newContact.jsx
@@ -74,7 +74,7 @@ const NewContactModal = ({ closeModal }) => {
                   setSuccessMessage("");
                   closeModal();
                 }}
-                className="w-full text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="w-full text-white bg-orange-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 OK
               </button>
