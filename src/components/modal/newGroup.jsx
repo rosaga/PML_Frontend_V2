a6410b735c223125f6c97b115b5aadc4d6b84c1e
vsidera <siderravictor@gmail.com>
2024-06-13 15:18:36 +0300

    fix search

--- src/components/modal/newGroup.jsx
+++ src/components/modal/newGroup.jsx
@@ -90,7 +90,7 @@ const NewGroupModal = ({ closeModal }) => {
                   setSuccessMessage("");
                   closeModal();
                 }}
-                className="w-full text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="w-full text-white bg-orange-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 OK
               </button>
