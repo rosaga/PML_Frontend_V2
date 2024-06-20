ce55c5fccd79406084a2122e6d7d07d6a9975cf8
rosaga <richardjohnzranger@gmail.com>
2024-06-20 15:16:50 +0300

    update of groupDetails

--- src/app/api/actions/group/group.js
+++ src/app/api/actions/group/group.js
@@ -75,3 +75,37 @@ export async function groupCreate(formValues) {
       };
     }
   }
+  export async function GetGroupDetails(org_id,group_id,page,pageSize) {
+
+    let groupUrl
+    if (page || pageSize) {
+     groupUrl = `${apiUrl.GET_CONTACTS}/${org_id}/group/contact?eq__group_id=${group_id}&size=${pageSize}&page=${page}`;
+    }else{
+      groupUrl = `${apiUrl.GET_CONTACTS}/${org_id}/group/contact`;
+    }
+  
+    try {
+      const config = await authHeaders();
+  
+      const res = await axios.get(groupUrl, config);
+  
+      if (res.data && res.status === 200) {
+        console.log("THE RESPONSE IS !!!!!!!", res);
+      }
+  
+      return res;
+    } catch (error) {
+      if (error.response) {
+        return {
+          errors: {
+            _error: 'The contacts could not be returned.',
+          },
+        };
+      }
+      return {
+        errors: {
+          _error: 'Network error. Please try again.',
+        },
+      };
+    }
+  }
