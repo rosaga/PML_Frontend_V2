6f0df6f8cb589c8f92f52dc40a8a12890767f731
vsidera <siderravictor@gmail.com>
2024-09-19 13:49:14 +0300

    fix sending sms

--- src/app/apps/sms/messages/page.jsx
+++ src/app/apps/sms/messages/page.jsx
@@ -2,15 +2,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Box from "@mui/material/Box";
-import { DataGrid, GridRowsProp, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridToolbar,
+} from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
 import IosShareIcon from "@mui/icons-material/IosShare";
-import AddIcon from '@mui/icons-material/Add';
+import AddIcon from "@mui/icons-material/Add";
 import PeakButton from "../../../../components/button/button";
-import PeakSearch from "../../../../components/search/search"
-import InviteUserModal from "../../../../components/modal/inviteUser"
-import SendSmsModal from "../../../../components/modal/sendSms"
-import SendBulkModal from "../../../../components/modal/sendBulkSms"
+import PeakSearch from "../../../../components/search/search";
+import InviteUserModal from "../../../../components/modal/inviteUser";
+import SendSmsModal from "../../../../components/modal/sendSms";
+import SendBulkModal from "../../../../components/modal/sendBulkSms";
 import apiUrl from "../../../api/utils/apiUtils/apiUrl";
 import { messagesAction } from "../../../api/actions/messages/messagesAction";
 import { getToken } from "@/utils/auth";
@@ -18,8 +23,8 @@ import { getToken } from "@/utils/auth";
 const Messages = () => {
   let org_id = null;
   let token = null;
-  if (typeof window !== 'undefined') {
-    org_id = localStorage.getItem('selectedAccountId');
+  if (typeof window !== "undefined") {
+    org_id = localStorage.getItem("selectedAccountId");
     token = getToken();
   }
 
@@ -28,9 +33,15 @@ const Messages = () => {
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchParams, setSearchParams] = useState({});
+  const [total, setTotal] = useState(0);
+
+  const [paginationModel, setPaginationModel] = React.useState({
+    pageSize: 10,
+    page: 0,
+  });
 
   const [page, setPage] = useState(0);
-  const [limit, setLimit] = useState(10)
+  const [limit, setLimit] = useState(10);
 
   const openSendSingle = () => {
     setIsSingleModalOpen(true);
@@ -59,29 +70,28 @@ const Messages = () => {
     setSearchParams({});
   };
 
-
   const getMessages = () => {
     if (org_id) {
-    messagesAction({org_id,page,limit})
-      .then((res) => {
-        if (res.errors) {
-          console.log("AN ERROR HAS OCCURED")
-        } else {
-          setMessages(res.data)
-          setLoading(false)
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-      });
+      messagesAction({ org_id, page, limit })
+        .then((res) => {
+          if (res.errors) {
+            console.log("AN ERROR HAS OCCURED");
+          } else {
+            setMessages(res.data);
+            setLoading(false);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     } else {
       console.log("org_id is null or undefined. Skipping API call.");
-    }  
+    }
   };
 
   useEffect(() => {
     getMessages();
-  }, [isSingleModalOpen,isBulkModalOpen, searchParams]);
+  }, [isSingleModalOpen, isBulkModalOpen, searchParams]);
 
   const columns = [
     { field: "source", headerName: "SOURCE", flex: 1, minWidth: 150 },
@@ -101,7 +111,12 @@ const Messages = () => {
             <div className="flex items-center justify-between">
               <p className="mt-4 font-medium text-lg">Users</p>
               <div className="ml-auto flex space-x-4">
-              <PeakSearch filterOptions={filterOptions} selectedFilter="" onSearch={handleSearch} onClearSearch={handleClearSearch}/>
+                <PeakSearch
+                  filterOptions={filterOptions}
+                  selectedFilter=""
+                  onSearch={handleSearch}
+                  onClearSearch={handleClearSearch}
+                />
                 <PeakButton
                   buttonText="Send SMS"
                   icon={AddIcon}
@@ -118,26 +133,25 @@ const Messages = () => {
             </div>
 
             <div className="mt-4">
-              <div style={{ height: 350, width: "100%" }}>
-
-                {loading ? (
-                  <p>Loading...</p>
-                ) : (
-                  <DataGrid
-                    rows={messages}
-                    columns={columns}
-                    sx={{
-                      "& .MuiDataGrid-columnHeader": {
-                        backgroundColor: "#F1F2F3",
-                      },
-                      "&.MuiDataGrid-root": {
-                        border: "none",
-                      },
-                    }}
-                    slots={{ toolbar: GridToolbar }}
-                  />
-                )}
-
+              <div style={{ width: "100%" }}>
+                <DataGrid
+                  rows={messages}
+                  columns={columns}
+                  loading={loading}
+                  paginationModel={paginationModel}
+                  onPaginationModelChange={setPaginationModel}
+                  rowCount={total}
+                  paginationMode="server"
+                  sx={{
+                    "& .MuiDataGrid-columnHeader": {
+                      backgroundColor: "#F1F2F3",
+                    },
+                    "&.MuiDataGrid-root": {
+                      border: "none",
+                    },
+                  }}
+                  slots={{ toolbar: GridToolbar }}
+                />
               </div>
             </div>
           </div>
