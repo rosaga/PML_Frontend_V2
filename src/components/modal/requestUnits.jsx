95683b227d038048db32e3c83c855d1219cc062d
Ted Aringo <tedaringo@Teds-MacBook-Pro.local>
2024-12-03 22:11:29 +0300

    Added more options to the request units

--- src/components/modal/requestUnits.jsx
+++ src/components/modal/requestUnits.jsx
@@ -184,9 +184,11 @@ const RequestUnitsModal = ({ closeModal }) => {
                       <option value="100">100</option>
                       <option value="150">150</option>
                       <option value="200">200</option>
+                      <option value="250">250</option>
                       <option value="500">500</option>
-                      <option value="1024">1024</option>
-                      <option value="3072">3072</option>
+                      <option value="1000">1000</option>
+                      <option value="5000">5000</option>
+                      <option value="10000">10000</option>
                       <option value="AIRTIME">AIRTIME</option>
                     </select>
                   </div>
