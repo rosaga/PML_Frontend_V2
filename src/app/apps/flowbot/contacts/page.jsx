b88913ad530cfc631e829513b9b4806ce27b8b37
rosaga <richardjohnzranger@gmail.com>
2025-05-09 12:01:30 +0300

    update to remove contact groups

--- src/app/apps/flowbot/contacts/page.jsx
+++ src/app/apps/flowbot/contacts/page.jsx
@@ -15,9 +15,9 @@ const DataRewards = () => {
             <div className="p-4">
 
 
-              {active === "contacts" && (
+             
                 <>
-                  <div className="flex flex-col sm:flex-row rounded-lg mt-2 border-[1.5px] mb-2">
+                  {/* <div className="flex flex-col sm:flex-row rounded-lg mt-2 border-[1.5px] mb-2">
                     <div className="m-2 flex-1">
                       <span
                         onClick={() => setChildActive("recipients")}
@@ -30,29 +30,13 @@ const DataRewards = () => {
                         Contacts
                       </span>
                     </div>
-                    <div className="m-2 flex-1">
-                      <span
-                        onClick={() => setChildActive("groups")}
-                        className={`flex-1 flex justify-center text-center ${
-                          childActive === "groups"
-                            ? "text-[#E88A17] bg-white border-[1.5px] border-[#E88A17] py-1 px-4 sm:px-8 rounded cursor-pointer"
-                            : "bg-[#F1F2F3] py-1 px-4 sm:px-8 rounded cursor-pointer"
-                        }`}
-                      >
-                        Groups
-                      </span>
-                    </div>
-                  </div>
+                  </div> */}
 
-                  {childActive === "recipients" && (
+                   
                     <RecipientsTable />
-                  )}
+                  
 
-                  {childActive === "groups" && (
-                    <GroupsTable />
-                  )}
                 </>
-              )}
             </div>
           </div>
         </div>
