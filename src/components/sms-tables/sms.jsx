2032b604c663cd5776d2eccfe81db0e982883bd5
siderra <siderravictor@gmail.com>
2025-07-16 07:57:46 +0300

    campaigns tab

---
+++ src/components/sms-tables/sms.jsx
@@ -0,0 +1,199 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridToolbar,
+} from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import IosShareIcon from "@mui/icons-material/IosShare";
+import AddIcon from "@mui/icons-material/Add";
+import PeakButton from "../../components/button/button";
+import PeakSearch from "../../components/search/search";
+import SendSmsModal from "../../components/modal/sendSms";
+import SendBulkModal from "../../components/modal/sendBulkSms";
+import { format,parseISO } from "date-fns";
+import { messagesAction } from "../../app/api/actions/messages/messagesAction";
+import { getToken } from "@/utils/auth";
+
+const MessagesTable = () => {
+  let org_id = null;
+  let token = null;
+  if (typeof window !== "undefined") {
+    org_id = localStorage.getItem("selectedAccountId");
+    token = getToken();
+  }
+
+  const [isSingleModalOpen, setIsSingleModalOpen] = useState(false);
+  const [isBulkModalOpen, setIsBulkModalOpen] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchParams, setSearchParams] = useState({});
+  const [total, setTotal] = useState(0);
+
+  const [paginationModel, setPaginationModel] = React.useState({
+    pageSize: 10,
+    page: 0,
+  });
+
+  const openSendSingle = () => {
+    setIsSingleModalOpen(true);
+  };
+
+  const openSendBulk = () => {
+    setIsBulkModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsSingleModalOpen(false);
+    setIsBulkModalOpen(false);
+  };
+
+  const filterOptions = [
+    { value: "ilike__firstName", label: "First Name" },
+    { value: "ilike__lastName", label: "Last Name" },
+    { value: "ilike__email", label: "Email" },
+  ];
+
+  const handleSearch = (filter, value) => {
+    setSearchParams({ [filter]: value });
+  };
+
+  const handleClearSearch = () => {
+    setSearchParams({});
+  };
+
+
+  const getMessages = () => {
+    if (!org_id) {
+      console.warn("org_id is null or undefined. Skipping API call.");
+      return;
+    }
+
+    setLoading(true);
+    messagesAction({
+      org_id,
+      page:  1,
+      limit: 20,
+      ...searchParams,
+    })
+      .then((res) => {
+        if (res?.errors) {
+          console.error(res.errors);
+        } else {
+          setMessages(res.data || []);
+        }
+      })
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    getMessages();
+  }, [isSingleModalOpen, isBulkModalOpen, searchParams]);
+
+  const columns = [
+    // { field: "id", headerName: "ID", flex: 1, minWidth: 50 },
+    { field: "source", headerName: "SOURCE", flex: 1, minWidth: 150 },
+    { 
+      field: "destination", 
+      headerName: "DESTINATION", 
+      flex: 1, 
+      minWidth: 150,
+      renderCell: (params) => (
+        <span style={{ fontWeight: '550' }}>
+          {params.value}
+        </span>
+      ) 
+    },
+    { field: "content", headerName: "CONTENT", flex: 1, minWidth: 500 },
+    { field: "channel", headerName: "CHANNEL", flex: 1, minWidth: 150 },
+    { field: "direction", headerName: "DIRECTION", flex: 1, minWidth: 150 },
+    { 
+      field: "status_desc", 
+      headerName: "STATUS", 
+      flex: 1, 
+      minWidth: 150, 
+      renderCell: (params) => {
+        let color = 'inherit'; // Default color
+        if (params.value === "SUCCESS") {
+          color = 'green';
+        } else if (params.value === "InvalidMsisdn") {
+          color = 'red';
+        }
+        return <span style={{ color }}>{params.value}</span>;
+      }
+    },
+    { field: "createdat", headerName: "Date Created", flex: 1, minWidth: 150, 
+      valueFormatter: (params) => {
+        try {
+          const date = parseISO(params);
+          return format(date, "yyyy-MM-dd HH:mm");
+        } catch (error) {
+          return "Invalid Date";
+        }
+      },
+     },
+  ];
+
+  return (
+    <div className="p-4 sm:ml-64 h-screen ">
+      <div className="flex flex-col h-full">
+        <div className="flex flex-col">
+          <div className="p-4">
+            <div className="flex items-center justify-between">
+              <p className="mt-4 font-medium text-lg">Messages</p>
+              <div className="ml-auto flex space-x-4">
+                <PeakSearch
+                  filterOptions={filterOptions}
+                  selectedFilter=""
+                  onSearch={handleSearch}
+                  onClearSearch={handleClearSearch}
+                />
+                <PeakButton
+                  buttonText="Send SMS"
+                  icon={AddIcon}
+                  className="bg-[#090A29] text-gray-100 text-sm rounded-[2px] px-2 shadow-sm outline-none"
+                  onClick={openSendSingle}
+                />
+                <PeakButton
+                  buttonText="Send Bulk"
+                  icon={AddIcon}
+                  className="bg-[#090A29] text-gray-100 text-sm rounded-[2px] px-2 shadow-sm outline-none"
+                  onClick={openSendBulk}
+                />
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <div style={{ width: "100%" }}>
+              <DataGrid
+                rows={messages}
+                columns={columns}
+                loading={loading}
+                getRowId={(row) => row.id}
+                paginationModel={paginationModel}
+                onPaginationModelChange={setPaginationModel}
+                pageSizeOptions={[10]}
+                paginationMode="client"
+                sx={{
+                  "& .MuiDataGrid-columnHeader": { backgroundColor: "#F1F2F3" },
+                  "&.MuiDataGrid-root":         { border: "none" },
+                }}
+                slots={{ toolbar: GridToolbar }}
+              />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {isSingleModalOpen && <SendSmsModal closeModal={closeModal} />}
+      {isBulkModalOpen && <SendBulkModal closeModal={closeModal} />}
+    </div>
+  );
+};
+
+export default MessagesTable;
