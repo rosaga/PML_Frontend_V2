f7c031bd8be40d1269b4400f1bab7b00531c0b4d
rosaga <richardjohnzranger@gmail.com>
2025-08-19 16:54:31 +0300

    prototype for dashboards

--- src/app/apps/data/dashboard/page.tsx
+++ src/app/apps/data/dashboard/page.tsx
@@ -16,6 +16,7 @@ import GroupDashboard from "@/components/rewards-tables/groupDashboard";
 import { GetDashboardSummary, GetDataBalance } from "@/app/api/actions/dashboard/dashboard";
 import { useRouter } from "next/navigation";
 import CircularProgress from "@mui/material/CircularProgress";
+import TrendsVisualization from '@/components/dashboard/TrendsVisualization';
 
 interface RowData {
   id: number;
@@ -315,6 +316,14 @@ const Dashboard = () => {
                   </div>
                   <div className="text-2xl font-bold">0</div>
                 </div>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div className="p-4 shadow-md rounded-lg">
+                <TrendsVisualization 
+                  selectedYear={selectedYear}
+                  selectedMonth={selectedMonth}
+                />
               </div>
             </div>
 
