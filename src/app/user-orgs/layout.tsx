21e6c46b44a01677cb11e7028559dcb635369505
Your Name <aicode22147@gmail.com>
2026-07-26 03:57:16 +0300

    added download feature to the web page platform

--- src/app/user-orgs/layout.tsx
+++ src/app/user-orgs/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import SessionProviderWrapper from '@/components/SessionProviderWrapper';
+// @ts-ignore: side-effect import of CSS file without type declarations
 import "./user-orgs.css"; // Import any specific styles for the login page
 
 export default function LoginLayout({
